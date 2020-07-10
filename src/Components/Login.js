d35346039a504c5e308e53478ce304eedc656ae3
tjforeman <tylerforeman92@gmail.com>
2020-07-10 11:36:20 -0400

    sending firebase token to backend

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import axios from 'axios';
 import { firebase } from '../Helpers/index.js';
 const firebaseUser = require('firebase/app');
 require('firebase/auth');
@@ -10,6 +10,18 @@ function AuthCallback(props) {
     if (user) {
       let token = await user.getIdToken();
       localStorage.setItem('token', token);
+      axios
+        .post(`${process.env.REACT_APP_API}`, {
+          token: token,
+        })
+        .then((res) => {
+          console.log(res);
+          localStorage.setItem('token', token);
+        })
+        .catch((err) => {
+          console.log(err);
+          window.alert('There was an error singing in');
+        });
     }
   });
 }
