ddf86ae1b13b3ea93cc293c9b584eafed726ee5a
tjforeman <tylerforeman92@gmail.com>
2020-07-13 13:30:04 -0400

    moved token to headers

--- src/auth/axiosWithAuth.js
+++ src/auth/axiosWithAuth.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 export const axiosWithAuth = () => {
-  //   const token = localStorage.getItem("token");
+  const token = localStorage.getItem('token');
 
   return axios.create({
     headers: {
       'Content-Type': 'application/json',
-      //   Authorization: token
+      Authorization: token,
     },
     baseURL: `${process.env.REACT_APP_API}`,
   });
