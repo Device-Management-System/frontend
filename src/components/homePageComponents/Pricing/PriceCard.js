35b17a8664ffbd3c52011dfd22fe2a9f4046595d
guillsav <guillsav@gmail.com>
2020-07-21 11:44:05 -0400

    Revert "Revert "Remaning folders to lowercase""
    This reverts commit 016be1c52d6c72d47ce0baeaa58e1c6a01135d39.

---
+++ src/components/homePageComponents/Pricing/PriceCard.js
@@ -0,0 +1,76 @@
+import React, { Fragment, useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import CardBtn from '../../customComponents/CustomButton/CustomButton';
+
+import {
+  PricingCardSelect,
+  PricingCard,
+  PricingCardHeader,
+  PricingProduct,
+  Price,
+  Features,
+} from './Pricing.styles';
+
+const PriceCard = ({
+  title,
+  monthly_price,
+  annual_price,
+  features,
+  btn_label,
+  highlight,
+  isMonthly,
+}) => {
+  const [selected, setSelected] = useState(highlight);
+  useEffect(() => {
+    setSelected(selected);
+  }, [selected, setSelected]);
+
+  return (
+    <PricingCardSelect onClick={() => setSelected(!selected)}>
+      <PricingCard highlight={selected ? selected : false}>
+        <PricingCardHeader>
+          <PricingProduct>{title}</PricingProduct>
+          <Price>${!isMonthly ? annual_price : monthly_price}</Price>
+        </PricingCardHeader>
+        <Features>
+          {features.map((feature, id) => {
+            return (
+              <Fragment key={id}>
+                <p>
+                  <FontAwesomeIcon
+                    icon="check"
+                    style={{ marginRight: '1.1rem' }}
+                  />
+                  {feature}
+                </p>
+              </Fragment>
+            );
+          })}
+        </Features>
+        {selected ? (
+          <CardBtn pricingr="true" to="#">
+            {btn_label}
+          </CardBtn>
+        ) : (
+          <CardBtn pricing="true" to="#">
+            {btn_label}
+          </CardBtn>
+        )}
+      </PricingCard>
+    </PricingCardSelect>
+  );
+};
+
+PriceCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  monthly_price: PropTypes.string.isRequired,
+  annual_price: PropTypes.string.isRequired,
+  features: PropTypes.array.isRequired,
+  btn_label: PropTypes.string.isRequired,
+  highlight: PropTypes.bool.isRequired,
+  isMonthly: PropTypes.bool.isRequired,
+};
+
+export default PriceCard;
---
+++ src/views/Homepage/Homepage.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useContext } from 'react';
+import AuthContext from '../../context/auth/authContext';
+import { useHistory } from 'react-router-dom';
+import Billboard from '../../components/homePageComponents/Billboard/Billboard';
+import Pricing from '../../components/homePageComponents/Pricing/Pricing';
+import Team from '../../components/homePageComponents/Team/Team';
+import Contact from '../../components/homePageComponents/Contact/Contact';
+import Footer from '../../components/homePageComponents/Footer/Footer';
+
+import './Homepage.css';
+
+const Homepage = () => {
+  const history = useHistory();
+  const authContext = useContext(AuthContext);
+  const { currentUser } = authContext;
+
+  useEffect(() => {
+    if (currentUser) {
+      history.push('/manager-dashboard');
+    }
+  });
+
+  return (
+    <div className="home">
+      <Billboard />
+      <Pricing />
+      <Team />
+      <Contact />
+      <Footer />
+    </div>
+  );
+};
+
+export default Homepage;
