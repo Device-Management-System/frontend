016be1c52d6c72d47ce0baeaa58e1c6a01135d39
guillsav <guillsav@gmail.com>
2020-07-21 11:38:14 -0400

    Revert "Remaning folders to lowercase"
    This reverts commit 105701f7ce7bee822050683a2fe8ec4ac5f9b8d6.

--- src/components/customComponents/CustomButton/CustomButton.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import { CustomButtonContainer } from './CustomButtom.style';
-
-const CustomButton = (props) => (
-  <CustomButtonContainer {...props}>{props.children}</CustomButtonContainer>
-);
-
-export default CustomButton;
