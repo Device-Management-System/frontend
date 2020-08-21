9a2534b342bcc5513688297c600b0b193921c26b
guillsav <guillsav@gmail.com>
2020-08-21 13:40:57 -0400

    Added user profile check to Private and Admin routes

--- src/components/routing/PrivateRoute.js
+++ src/components/routing/PrivateRoute.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
+import { AuthContext } from '../../context/auth/AuthContext';
 import { Route, Redirect } from 'react-router-dom';
 import Layout from '../layout/Layout';
 
 const PrivateRoute = ({ children, ...otherProps }) => {
+  const authContext = useContext(AuthContext);
+  const { currentUser } = authContext;
   const { isAuthenticated } = useAuth0();
+  const [isCompleted, setIsCompleted] = useState(false);
+
+  useEffect(() => {
+    if (currentUser && currentUser.is_completed)
+      setIsCompleted(currentUser.is_completed);
+  }, [currentUser]);
+
   return (
     <Route
       {...otherProps}
-      render={() =>
-        isAuthenticated ? <Layout>{children}</Layout> : <Redirect to="/" />
-      }
+      render={() => {
+        if (isAuthenticated && currentUser && isCompleted) {
+          return <Layout>{children}</Layout>;
+        } else if (isAuthenticated && currentUser && !isCompleted) {
+          return (
+            <Layout>
+              <Redirect to="/update-profile" />
+            </Layout>
+          );
+        } else {
+          return <Redirect to="/" />;
+        }
+      }}
     ></Route>
   );
 };
--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,21 +1,7 @@
 import React, { useEffect, useContext } from 'react';
-import { useHistory } from 'react-router-dom';
-import { useAuth0 } from '@auth0/auth0-react';
-import { AuthContext } from '../../context/auth/AuthContext';
 // import PostDevice from '../../components/PostDevice';
 
 const Dashboard = () => {
-  const history = useHistory();
-  const { isAuthenticated } = useAuth0();
-  const authContext = useContext(AuthContext);
-  const { currentUser } = authContext;
-
-  useEffect(() => {
-    if (!isAuthenticated) history.push('/login');
-    if (currentUser && !currentUser.is_completed)
-      history.push('/update-profile');
-  }, [history, isAuthenticated, currentUser]);
-
   return (
     <div>
       Dashboard
